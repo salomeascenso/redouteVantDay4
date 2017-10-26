a430ad9e0a0e438ccdbf0700732c0b87b79a3415
dev <dev@DevVM-ip100202-v12>
2017-10-26 09:43:58 +0100

    Com a classe ValidarLogin

---
+++ src/com/vantagem/gitproj/ValidarLogin.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vantagem.gitproj;
+
+/**
+ *
+ * @author dev
+ */
+public class ValidarLogin {
+    
+    String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    
+}
